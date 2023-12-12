739009bbf034a653d29405440f15ae13d5de4f70
admin <a1899783@Ubuntu22.04.myguest.virtualbox.org>
2023-12-12 15:39:15 +1030

    added offline and online training files for PG algorithm

---
+++ run_rllib_offline.py
@@ -0,0 +1,17 @@
+from ray.rllib.algorithms.pg import PGConfig
+from ray.tune.logger import pretty_print
+
+algo = (
+    PGConfig()
+    .environment(env="CartPole-v1")
+    .framework("torch")
+    .offline_data(output="/tmp/cartpole-out", output_max_file_size=5000000)
+    .build())
+
+for i in range(10):
+    result = algo.train()
+    print(pretty_print(result))
+
+    if i % 5 == 0:
+        checkpoint_dir = algo.save().checkpoint.path
+        print(f"Checkpoint saved in directory {checkpoint_dir}")
